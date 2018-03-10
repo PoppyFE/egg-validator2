007873685d7369c095d821bf83d540e09dbc42db
alex zhang <gzane24@gmail.com>
2018-03-11 03:44:17 +0800

    add isValid

--- app.js
+++ app.js
@@ -9,4 +9,14 @@ const customerValidator = {
 module.exports = app => {
   const parameter = new Parameter(app.config.validator2);
   app.validator = Object.assign(parameter, customerValidator, validator);
+  app.validator.isValid = (rules, obj) => {
+    try {
+      const errMsg = parameter.validate(rules, obj);
+      if (errMsg !== undefined) return false;
+    } catch (err) {
+      return false;
+    }
+
+    return true;
+  };
 };
