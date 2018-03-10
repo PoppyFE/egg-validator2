0bc0e9b8d4f41073029e47504b3623c6906b2edf
alex zhang <gzane24@gmail.com>
2018-03-11 03:34:55 +0800

    add parameter

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const validator = require('validator');
+const Parameter = require('parameter');
 
 const customerValidator = {
 };
 
 module.exports = app => {
-  app.validator = Object.assign(customerValidator, validator);
+  const parameter = new Parameter(app.config.validator2);
+  app.validator = Object.assign(parameter, customerValidator, validator);
 };
