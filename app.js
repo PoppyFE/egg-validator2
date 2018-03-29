7ff13d287270cd4f8c5a38c8228b5895ecd27934
alex zhang <gzane24@gmail.com>
2018-03-29 11:54:07 +0800

    添加身份证名字

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Parameter = require('parameter');
 // 8-20 必须包含至少一个
 const passwrodRegexp = new RegExp('^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[#@!~%^&*])[a-zA-Z\\d#@!~%^&*].{7,19}$');
 const idNoReg = /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$)/;
+const idNameReg = /^[\u4e00-\u9fa5]+(·|\.)?[\u4e00-\u9fa5]$/;
 
 const customerValidator = {
   isValidComplexPassword(val) {
@@ -14,6 +15,10 @@ const customerValidator = {
 
   isValidIdNo(val) {
     return idNoReg.test(val);
+  },
+
+  isValidIdName(val) {
+    return idNameReg.test(val);
   },
 };
 
