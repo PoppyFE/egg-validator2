d20a7adac49590d349f30b327101c1433e638453
alex zhang <gzane24@gmail.com>
2018-03-29 11:25:22 +0800

    添加身份证验证

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const Parameter = require('parameter');
 
 // 8-20 必须包含至少一个
 const passwrodRegexp = new RegExp('^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[#@!~%^&*])[a-zA-Z\\d#@!~%^&*].{7,19}$');
+const idNoReg = /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$)/;
 
 const customerValidator = {
   isValidComplexPassword(val) {
     return passwrodRegexp.test(val);
+  },
+
+  isValidIdNo(val) {
+    return idNoReg.test(val);
   },
 };
 
