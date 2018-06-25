d22fbb614fff284ab5d8bce482911208e9abde1b
alex zhang <gzane24@gmail.com>
2018-06-25 11:01:04 +0800

    fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const GB2260 = require('id-validator/src/GB2260');
 // 8-20 必须包含至少一个
 const passwrodRegexp = new RegExp('^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[#@!~%^&*])[a-zA-Z\\d#@!~%^&*].{7,19}$');
 const idNoReg = /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$)/;
-const idNameReg = /^[\u4e00-\u9fa5]+(·|\.)?[\u4e00-\u9fa5]$/;
+const idNameReg = /^[\u4e00-\u9fa5]+(·|\.)?[\u4e00-\u9fa5]*[\u4e00-\u9fa5]$/;
 
 const customerValidator = {
   isValidComplexPassword(val) {
